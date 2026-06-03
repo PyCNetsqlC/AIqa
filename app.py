9583fee58812d2c79f8bf66abe8ac849add8e33f
PyCNetsqlC <169810770+PyCNetsqlC@users.noreply.github.com>
2026-06-03 19:32:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ time=0
 
 if time==0:
     returnnal()
-    print(f"資料庫資料更新中,time:{time}"）
+    print(f"資料庫資料更新中,time:{time}")
 
 with open("./db_file/clean.csv", mode="r", encoding="utf-8-sig",newline="") as f:
     csvfile = csv.reader(f)
